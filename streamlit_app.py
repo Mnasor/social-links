33ec785eeffbabf88dba3a857fc9c363a603c0b9
Mansour <mnasor3000@outlook.sa>
2022-10-11 18:07:00 +0300

    first update

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,21 @@ col2.image(Image.open('dp.png'))
 
 st.header('Chanin Nantasenamat, Ph.D.')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('this is my first link ')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('youtube', 'https://www.youtube.com/watch?v=uY0p1M8AB_4&t=11s',
+          'Data Professor YouTube channel', icon_size)
+st_button('youtube', 'https://youtube.com/codingprofessor',
+          'Coding Professor YouTube channel', icon_size)
+st_button('medium', 'https://data-professor.medium.com/',
+          'Read my Blogs', icon_size)
+st_button('twitter', 'https://twitter.com/thedataprof/',
+          'Follow me on Twitter', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/',
+          'Follow me on LinkedIn', icon_size)
+st_button('newsletter', 'https://sendfox.com/dataprofessor/',
+          'Sign up for my Newsletter', icon_size)
+st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/',
+          'Buy me a Coffee', icon_size)
