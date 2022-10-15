473c666b50951ef89dea8ab6beb8c3da4fb6ea9b
Mansour <mnasor3000@outlook.sa>
2022-10-15 11:56:02 +0300

    last links

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,20 +11,16 @@ col2.image(Image.open('dp.png'))
 
 st.header('al Yousef')
 
-st.info('this is my first link ')
+st.info('نقدم محتوى على شكل فلوقات او فعاليات  ')
 
 icon_size = 20
 
 st_button('youtube', 'https://youtube.com/channel/UCDXLeBjO7av7mcJ25uAE3ew',
           'قناة اليوتيوب', icon_size)
 
-st_button('medium', 'https://www.snapchat.com/add/f-alshehili?share_id=MEI2MzgxNEMtRDM2NC00NzQ4LTg5NkMtRUQ3QkY2NUM5Nzc4&locale=ar_SA@calendar=gregorian',
-          'ٍSnapchat', icon_size)
+st_button('snapchat', 'https://www.snapchat.com/add/f-alshehili?share_id=MEI2MzgxNEMtRDM2NC00NzQ4LTg5NkMtRUQ3QkY2NUM5Nzc4&locale=ar_SA@calendar=gregorian',
+          'سناب تشات', icon_size)
 st_button('twitter', 'https://twitter.com/thedataprof/',
           '   Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/',
-          'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/',
-          'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/',
-          'Buy me a Coffee', icon_size)
+st_button('tiktok', 'https://www.tiktok.com/@fahad_childern?_t=8WQJi2SuYwk&_r=1',
+          'تيك توك', icon_size)
